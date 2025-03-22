230e659390f70b2005ee5e14eeb03c936f91ae08
Thomas Black <tomjblack250@gmail.com>
2025-03-22 15:44:32 -0700

    added daisyUi for quick styles. Added some more robust shadcn components. added some nice rr7 snippets

---
+++ app/routes/buttons.tsx
@@ -0,0 +1,35 @@
+export default function Buttons() {
+  return (
+    <div className="container mb-48 mt-36 flex flex-col items-center justify-center gap-6">
+      <h1 className="text-h1">Buttons</h1>
+      <main>
+        <div className="flex w-full flex-wrap items-center justify-center gap-4 delay-200">
+          <button className="btn">Click Me!</button>
+          <button className="btn btn-accent">Click Me!</button>
+          <button className="btn btn-active">Click Me!</button>
+          <button className="btn btn-block">Click Me!</button>
+          <button className="btn btn-circle">Click Me!</button>
+          <button className="btn btn-dash">Click Me!</button>
+          <button className="btn btn-error">Click Me!</button>
+          <button className="btn btn-ghost">Click Me!</button>
+          <button className="btn btn-info">Click Me!</button>
+          <button className="btn btn-lg">Click Me!</button>
+          <button className="btn btn-link">Click Me!</button>
+          <button className="btn btn-md">Click Me!</button>
+          <button className="btn btn-neutral">Click Me!</button>
+          <button className="btn btn-outline">Click Me!</button>
+          <button className="btn btn-primary">Click Me!</button>
+          <button className="btn btn-secondary">Click Me!</button>
+          <button className="btn btn-sm">Click Me!</button>
+          <button className="btn btn-soft">Click Me!</button>
+          <button className="btn btn-square">Click Me!</button>
+          <button className="btn btn-success">Click Me!</button>
+          <button className="btn btn-warning">Click Me!</button>
+          <button className="btn btn-wide">Click Me!</button>
+          <button className="btn btn-xl">Click Me!</button>
+          <button className="btn btn-xs">Click Me!</button>{" "}
+        </div>
+      </main>
+    </div>
+  );
+}
